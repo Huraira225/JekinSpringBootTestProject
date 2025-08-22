1cab0a56435b47caf09f2cf36a47ea906ef3fd0f
Huraira225 <huraira.tasawer786@gmail.com>
2025-08-22 11:06:35 -0500

    SpringBoot Test Project

---
+++ src/main/java/com/testJenkins/JekinsProject/JekinsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.testJenkins.JekinsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JekinsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JekinsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testJenkins/JekinsProject/JekinsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testJenkins.JekinsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JekinsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
